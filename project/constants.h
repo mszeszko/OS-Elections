7c28cfd86b6b6cbfb052a3bde7b73df8d8a6bab3
Maciej <m.szeszko@student.uw.edu.pl>
2014-12-20 03:18:33 +0100

    Basic IPC oriented structures + constants.

---
+++ project/constants.h
@@ -0,0 +1,32 @@
+/*
+     Maciej Szeszko,
+     id: ms335814,
+     University of Warsaw
+*/
+
+#ifndef __CONSTANTS_H_
+#define __CONSTANTS_H_
+
+/*
+   IPC message queues keys.
+*/
+#define INIT_CONNECTION_IPC_QUEUE_KEY 1234L
+#define COMMITTEE_DATA_IPC_QUEUE_KEY 1235L
+#define FINISH_IPC_QUEUE_KEY 1236L
+#define RAPORT_IPC_QUEUE_KEY 1237L
+
+/*
+   IPC message constant message types.
+*/
+#define INIT_CONNECTION_MESSAGE_TYPE 0L
+#define SEND_RAPORT_REQUEST_MESSAGE_TYPE 0L
+#define READ_RAPORT_MESSAGE_TYPE 1L
+
+/*
+   System constraints.
+*/
+#define MAX_DEDICATED_COMMITTEE_SERVER_THREADS 100
+#define MAX_LISTS 100
+#define MAX_CANDIDATES_PER_LIST 100
+
+#endif
---
+++ project/message_structures.h
@@ -0,0 +1,108 @@
+/*
+			Maciej Szeszko
+			id: ms335814,
+			University of Warsaw
+*/
+
+#ifndef __MESSAGE_STRUCTURES_H_
+#define __MESSAGE_STRUCTURES_H_
+
+#include "constants.h"
+
+/* ************** */
+/* IPC STRUCTURES */
+/* ************** */
+
+/* 
+	 Message send by committee to initiate connection with server.
+   As a response server returns appropriately set `ack` field,
+   that represents whether connection has been successfuly initiated or not. 
+*/
+typedef struct {
+  long operationId;
+  char ack;
+} initialConnectionMessage;
+
+
+/*
+   Basic local committee information.
+*/
+typedef struct {
+  unsigned int eligibledVoters;
+  unsigned int totalVotes;
+} basicCommitteeInfo;
+
+
+/*
+   Data message structure. Send by committee to dedicated server thread.
+   Note:
+   -> `localInfo`: is set only when sending first chunk of data through IPC.
+   -> `finished`: set as the only message after whole data has been processed.
+*/
+typedef struct {
+  long operationId;
+  unsigned int list;
+  unsigned int candidate;
+  unsigned int validVotes;
+  basicCommitteeInfo localInfo;
+	char finished; 
+} committeeMessage;
+
+
+/*
+   Acknowledgment that last chunk of data from particular committee process
+   has been just processed.
+*/
+typedef struct {
+  long operationId;
+  unsigned int processedMessages;
+  unsigned int validVotes;
+} serverAckMessage;
+
+
+/*
+   Report header structure.
+*/
+typedef struct {
+  unsigned int processedCommittees;
+  unsigned int committees;
+  unsigned int validVotes;
+  unsigned int invalidVotes;
+  unsigned int eligibledVoters;
+} reportHeader;
+
+
+/*
+   Single list report structure.
+*/
+typedef struct {
+  unsigned int list;
+  unsigned int candidates;
+  unsigned int candidateVotes[MAX_CANDIDATES];
+} singleListReport;
+
+
+/*
+   Report message structures.
+   -> `getReportMessage`: sent by `raport` process to get back report
+      for specific votelist(or eventually all of them).
+   -> `singleListReportMessage`: represents report for specified list.
+   -> `allListReportMessage`: report for all lists.
+*/
+typedef struct {
+  long operationId;
+  long reportList;
+} getReportMessage;
+
+typedef struct {
+  long operationId;
+  singleListReport listReport;
+} singleListReportMessage;
+
+typedef struct {
+  long operationId;
+  unsigned int lists;
+  singleListReport allReports[MAX_LISTS];
+} allListReportMessage;
+
+#endif
